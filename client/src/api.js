06638701a4aa432611daa324c877662ba3f19642
wrapup <tes1.acc001@gmail.com>
2026-05-31 00:50:36 +0530

    Update API base URL to production server

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://wrapup-server-dslb.onrender.com/api",
 });
 
 export const getNotes = (url, model) =>
